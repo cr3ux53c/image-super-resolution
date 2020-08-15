87d47dbb1e6438f03206f1bde4389067be8486f1
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-08-15 10:20:04 +0000

    모든 모델 적용

--- run.py
+++ run.py
@@ -1,16 +1,14 @@
 import isr
 import numpy as np
 from PIL import Image
-from ISR.models import RDN
+from ISR.models import RDN, RRDN
 
 lr_img = np.array(Image.open('data/input/sample/baboon.png'))
 
-model = RDN(
-    weights='psnr-small'
-    # weights='psnr-large'
-    # weights='noise-cancel'
-    # weights='gans'
-)
+model = RDN(weights='psnr-small')
+# model = RDN(weights='psnr-large')
+# model = RDN(weights='noise-cancel')
+# model = RRDN(weights='gans')
 
 sr_img = model.predict(lr_img, by_patch_of_size=50)
 Image.fromarray(sr_img).save('test.png')
