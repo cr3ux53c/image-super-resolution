b6fe7656ce1d9a4fcf7aabe6f99758d0b310a976
LIM HYUN SEOK <cr3ux53c@gmail.com>
2020-08-15 14:02:59 +0000

    모든 데이터셋 적용

--- run.py
+++ run.py
@@ -1,14 +1,46 @@
 import isr
+import sys
+import os
+from os.path import join
 import numpy as np
+import glob
 from PIL import Image
 from ISR.models import RDN, RRDN
 
-lr_img = np.array(Image.open('data/input/sample/baboon.png'))
+model_list = [
+    RDN(weights='psnr-small'),
+    RDN(weights='psnr-large'),
+    RDN(weights='noise-cancel'),
+    RRDN(weights='gans'),
+]
 
-model = RDN(weights='psnr-small')
-# model = RDN(weights='psnr-large')
-# model = RDN(weights='noise-cancel')
-# model = RRDN(weights='gans')
+dataset_list = [
+    join('data', 'input', 'chest_xray', 'test', 'NORMAL'),
+    join('data', 'input', 'chest_xray', 'test', 'PNEUMONIA'),
+    join('data', 'input', 'chest_xray', 'train', 'NORMAL'),
+    join('data', 'input', 'chest_xray', 'train', 'PNEUMONIA'),
+    join('data', 'input', 'chest_xray', 'val', 'NORMAL'),
+    join('data', 'input', 'chest_xray', 'val', 'PNEUMONIA'),
+]
 
-sr_img = model.predict(lr_img, by_patch_of_size=50)
-Image.fromarray(sr_img).save('test.png')
+result_dir = 'results/chest_xray'
+
+resize_list = [
+    256,
+    700,
+    1000,
+]
+
+for mod_idx, model in enumerate(model_list):
+    for dataset_dir in dataset_list:
+        for size in resize_list:
+            data_list = glob.glob(join(dataset_dir + '_' + str(size), '*.png'))
+
+            for data_path in data_list:
+                save_dir = join(result_dir, '_'.join(os.path.dirname(data_path).split('/')[3:5]) + '_mod' + str(mod_idx))
+                if not os.path.isdir(save_dir):
+                    os.makedirs(save_dir, exist_ok=True)
+
+                sr_img = model.predict(np.array(Image.open(data_path)), by_patch_of_size=50)
+                Image.fromarray(sr_img).save(join(save_dir, os.path.splitext(os.path.basename(data_path))[0] + '.png'))
+                print('Super-resolutioning... model_num: {:1}, src: {}'.format(mod_idx, data_path))
